1e85719fbd476af633d9ff62382fb4807b47756d
kirill-misiuk <kirill.misiuk@intexsoft.by>
2020-12-08 12:35:46 +0300

    Updates

---
+++ precache-manifest.2a86cf5b7aaeaea0e2b674b3c1c92dae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba52aead92d438a8333",
+    "url": "/static/css/main.be2d9920.chunk.css"
+  },
+  {
+    "revision": "dba52aead92d438a8333",
+    "url": "/static/js/main.591a67ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c0a1e57f20a2ae67ceb",
+    "url": "/static/css/2.c93de4dd.chunk.css"
+  },
+  {
+    "revision": "5c0a1e57f20a2ae67ceb",
+    "url": "/static/js/2.7200b1b7.chunk.js"
+  },
+  {
+    "revision": "2b8196a51fd641948d8c31ba25b52db0",
+    "url": "/static/media/wave.2b8196a5.svg"
+  },
+  {
+    "revision": "65f03140d39415a8814dcce03542c7d3",
+    "url": "/static/media/play.65f03140.svg"
+  },
+  {
+    "revision": "a61933e8724eb3f6190514bdd30baadc",
+    "url": "/static/media/pause.a61933e8.svg"
+  },
+  {
+    "revision": "c1f419d6abb1c32f97508b61040b2074",
+    "url": "/static/media/readed.c1f419d6.svg"
+  },
+  {
+    "revision": "3c841bf605a8a476802d71028127e33b",
+    "url": "/static/media/noreaded.3c841bf6.svg"
+  },
+  {
+    "revision": "00761eade88267404327c0d8f18853df",
+    "url": "/index.html"
+  }
+];
